13917fd956909e60907d7c130737001c3003327d
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2021-03-30 10:16:32 +0300

    feat: change database; VADIM add db-insertions

--- database/src/main/resources/gulashi.sql
+++ database/src/main/resources/gulashi.sql
@@ -1,32 +1,53 @@
-create table bigunets_mark
+create table tutor
 (
-    bigunets_no   integer     not null
-        constraint fk_bigunets
-            references bigunets
-            on update cascade on delete cascade,
-    student_code  integer     not null
-        constraint bigunets_mark_pk_2
-            primary key
-        constraint fk_student
-            references student
-            on update cascade on delete cascade,
-    vidomist_no   integer     not null
-        constraint fk_vidomist
-            references vidomist
-            on update cascade on delete cascade,
-    tutor_no      integer     not null
-        constraint fk_tutor
-            references tutor
-            on update cascade on delete cascade,
-    trim_mark     smallint    not null,
-    mark_check    smallint    not null,
-    complete_mark smallint    not null,
-    nat_mark      varchar(30) not null,
-    ects_mark     char(2)     not null,
-    constraint bigunets_mark_pk
-        unique (bigunets_no, vidomist_no)
+    tutor_no         serial      not null
+        constraint tutor_pk
+            primary key,
+    tutor_name       varchar(32) not null,
+    tutor_surname    varchar(32) not null,
+    tutor_patronymic varchar(32),
+    science_degree   varchar(20),
+    academ_status    varchar(20),
+    position         varchar(20)
 );
 
+create table "subject"
+(
+    subject_no  serial       not null
+        constraint subject_pk
+            primary key,
+    subject_name varchar(128) not null,
+    edu_level   char(8)      not null,
+    faculty     varchar(64)  not null
+);
+
+create table student
+(
+    student_code        serial      not null
+        constraint student_pk
+            primary key,
+    student_surname     varchar(32) not null,
+    student_name        varchar(32) not null,
+    student_patronymic  varchar(32),
+    student_record_book varchar(20)
+);
+
+
+
+create table "group"
+(
+    group_code serial      not null
+        constraint group_pk
+            primary key,
+    group_name varchar(16) not null,
+    edu_year   char(9)     not null,
+    trim       char(2)     not null,
+    course     integer     not null,
+    subject_no integer     not null
+        constraint fk_subject_no
+            references "subject"
+            on update cascade
+);
 
 create table bigunets
 (
@@ -42,60 +63,6 @@ create table bigunets
             references tutor
             on update cascade
 );
-
-create table subject
-(
-    subject_no  serial       not null
-        constraint subject_pk
-            primary key,
-    subject_name varchar(128) not null,
-    edu_level   char(8)      not null,
-    faculty     varchar(64)  not null
-);
-
-
-alter table bigunets
-    owner to postgres;
-
-
-alter table bigunets_mark
-    owner to postgres;
-
-alter table subject
-    owner to postgres;
-
-
-create table "group"
-(
-    group_code serial      not null
-        constraint group_pk
-            primary key,
-    group_name varchar(16) not null,
-    edu_year   char(9)     not null,
-    trim       char(2)     not null,
-    course     integer     not null,
-    subject_no integer     not null
-        constraint fk_subject_no
-            references subject
-            on update cascade
-);
-
-alter table "group"
-    owner to postgres;
-
-create table student
-(
-    student_code        serial      not null
-        constraint student_pk
-            primary key,
-    student_surname     varchar(32) not null,
-    student_name        varchar(32) not null,
-    student_patronymic  varchar(32),
-    student_record_book varchar(20)
-);
-
-alter table student
-    owner to postgres;
 
 create table vidomist
 (
@@ -117,45 +84,38 @@ create table vidomist
             on update cascade
 );
 
-alter table vidomist
-    owner to postgres;
-
-create table tutor
+create table bigunets_mark
 (
-    tutor_no         serial      not null
-        constraint tutor_pk
-            primary key,
-    tutor_name       varchar(32) not null,
-    tutor_surname    varchar(32) not null,
-    tutor_patronymic varchar(32),
-    science_degree   varchar(20),
-    academ_status    varchar(20),
-    position         varchar(20)
+    bigunets_no   integer     not null
+        constraint fk_bigunets
+            references bigunets
+            on update cascade on delete cascade,
+    student_code  integer     not null
+        constraint fk_student
+            references student
+            on update cascade on delete cascade,
+    vidomist_no   integer     not null
+        constraint fk_bigunets_mark_vidomist
+            references vidomist
+            on update cascade on delete cascade,
+    tutor_no      integer     not null
+        constraint fk_tutor
+            references tutor
+            on update cascade on delete cascade,
+    trim_mark     smallint    not null,
+    mark_check    smallint    not null,
+    complete_mark smallint    not null,
+    nat_mark      varchar(30) not null,
+    ects_mark     char(2)     not null,
+    constraint bigunets_mark_pk
+        unique (bigunets_no, vidomist_no, student_code, tutor_no)
 );
-
-alter table tutor
-    owner to postgres;
-
-create table student
-(
-    student_code        serial      not null
-        constraint student_pk
-            primary key,
-    student_surname     varchar(32) not null,
-    student_name        varchar(32) not null,
-    student_patronymic  varchar(32),
-    student_record_book varchar(20)
-);
-
-alter table student
-    owner to postgres;
-
 
 
 create table vidomist_mark
 (
     vidomist_no   integer     not null
-        constraint fk_vidomist
+        constraint fk_vidomist_mark_idomist
             references vidomist
             on update cascade on delete cascade,
     student_code  integer     not null
@@ -170,7 +130,3 @@ create table vidomist_mark
     constraint vidomist_mark_pk
         primary key (student_code, vidomist_no)
 );
-
-alter table vidomist_mark
-    owner to postgres;
-
