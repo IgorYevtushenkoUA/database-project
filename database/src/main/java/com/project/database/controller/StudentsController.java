b6cff2a7fc8bbff7aac0110d7180ba743caf69cc
BlaD200 <blad600@gmail.com>
2021-04-05 01:46:43 +0300

    Split the endpoint into students and debtors

--- database/src/main/java/com/project/database/controller/StudentsController.java
+++ database/src/main/java/com/project/database/controller/StudentsController.java
@@ -3,7 +3,10 @@ package com.project.database.controller;
 import com.project.database.entity.Student;
 import com.project.database.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,47 +24,49 @@ public class StudentsController {
 
 
     @GetMapping("/students")
-    public List<Student> getAll(
-            @RequestParam(name = "studentType", defaultValue = "student") String studentType, // думав стосовно boolean (чи студент чи боржник)
-            @RequestParam(name = "sortBy", defaultValue = "rating") String sortBy, // {surname, rating}
-            @RequestParam(name = "sortDesc", defaultValue = "true") Boolean sortDesc, // {high->low(desc);low->high(asc)}
-            @RequestParam(name = "page", defaultValue = "1") int page,
-            @RequestParam(name = "numberPerPage", defaultValue = "20") int numberPerPage
-    ) {
-        return studentService.findAll(page, numberPerPage);
-    }
-
-
-    @PostMapping("/students")
-    public List<Student> postAll(
+    public List<Student> getAllStudents(
             @RequestParam(name = "year", defaultValue = "2020") String year,
             @RequestParam(name = "subject", required = false) String subject,
             @RequestParam(name = "tutor", required = false) String tutor,
             @RequestParam(name = "group", required = false) String group,
             @RequestParam(name = "semester", required = false) Integer semester,
             @RequestParam(name = "course", required = false) Integer course,
-            @RequestParam(name = "studentType", defaultValue = "student") String studentType, // думав стосовно boolean (чи студент чи боржник)
             @RequestParam(name = "sortBy", defaultValue = "rating") String sortBy, // {surname, rating}
             @RequestParam(name = "sortDesc", defaultValue = "true") Boolean sortDesc, // {high->low(desc);low->high(asc)}
             @RequestParam(name = "page", defaultValue = "1") int page,
             @RequestParam(name = "numberPerPage", defaultValue = "20") int numberPerPage
     ) {
         List<Student> students;
-        students = studentType.equals("student")
-                ? studentService
-                .findAllByYearSubjectGroupTeacherTrimCourse(
-                        year, subject, group, tutor,
-                        String.valueOf(semester), String.valueOf(course),
-                        sortBy, sortDesc, page, numberPerPage
-                )
-                : studentService
-                .findAllDebtorsByYearSubjectGroupTeacherTrimCourse(
-                        year, subject, group, tutor,
-                        String.valueOf(semester), String.valueOf(course),
-                        sortBy, sortDesc, page, numberPerPage
-                );
+        students = studentService.findAllByYearSubjectGroupTeacherTrimCourse(
+                year, subject, group, tutor,
+                String.valueOf(semester), String.valueOf(course),
+                sortBy, sortDesc, page, numberPerPage
+        );
 
         return students;
+    }
+
+
+    @GetMapping("/debtors")
+    public List<Student> getAllDebtors(
+            @RequestParam(name = "year", defaultValue = "2020") String year,
+            @RequestParam(name = "subject", required = false) String subject,
+            @RequestParam(name = "tutor", required = false) String tutor,
+            @RequestParam(name = "group", required = false) String group,
+            @RequestParam(name = "semester", required = false) Integer semester,
+            @RequestParam(name = "course", required = false) Integer course,
+            @RequestParam(name = "sortBy", defaultValue = "rating") String sortBy, // {surname, rating}
+            @RequestParam(name = "sortDesc", defaultValue = "true") Boolean sortDesc, // {high->low(desc);low->high(asc)}
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "numberPerPage", defaultValue = "20") int numberPerPage
+    ) {
+        List<Student> debtors;
+        debtors = studentService.findAllDebtorsByYearSubjectGroupTeacherTrimCourse(
+                year, subject, group, tutor,
+                String.valueOf(semester), String.valueOf(course),
+                sortBy, sortDesc, page, numberPerPage
+        );
+        return debtors;
     }
 
 }
