96e95775926a242e9cbb5bbaca32ef7914a6c8b7
BlaD200 <blad600@gmail.com>
2021-04-11 13:22:58 +0300

    Added short info about subject for student dto

---
+++ database/src/main/java/com/project/database/dto/subject/SubjectShortInfo.java
@@ -0,0 +1,20 @@
+package com.project.database.dto.subject;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubjectShortInfo {
+    private Integer subjectID; // Example: 1,
+    private String subjectName; // Example: 'Технології сучасних дата - центрів',
+    private String tutorFullName; // Example: "Черкасов Дмитро Іванович",
+    private String group; // Example: "1",
+    private String controlType; // Example: "екзамен",
+    private String examDate; // Example: "2021-05-25",
+    private Integer grade; // Example: 90
+}
